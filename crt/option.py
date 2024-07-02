0db9bb9a3f4265755b79e846b93ab6bed3a60abf
feed228 <feed228new@gmail.com>
2024-07-02 13:30:21 +0300

    add correctedd create templaets

--- crt/option.py
+++ crt/option.py
@@ -3,7 +3,7 @@ from crt.templates import get_templates
 from crt.tools import get_all_directories, get_file_nesting
 
 
-def c_f(t_str_names,  ext, name=None):
+def c_f(t_str_names,  ext=None, name=None):
     if type(t_str_names) is dict:
         for key in t_str_names:
             if (name):
@@ -56,6 +56,21 @@ def c_f(t_str_names,  ext, name=None):
                 c_f(file, ext, full_name)
 
 
+def c_d(l_str_names):
+    print(l_str_names)
+    if type(l_str_names) is dict:
+        for key in l_str_names.keys():
+            l_str_names[key] = c_d(l_str_names[key])
+    else:
+        for index in range(len(l_str_names)):
+            if type(l_str_names[index]) is dict:
+                l_str_names[index] = c_d(l_str_names[index])
+            else:
+                if not '.' in l_str_names[index]:
+                    l_str_names[index] = {l_str_names[index]: []}
+    return l_str_names
+
+
 def crt_files2(ext, t_str_names):
     l_dir = get_file_nesting(t_str_names, len(t_str_names))
     print(l_dir)
@@ -89,15 +104,12 @@ def crt_dirs(t_str_names):
 
 
 def crt_temp(temp_name):
-    d_temp = get_templates(temp_name)
-    print(d_temp)
-    if d_temp:
-        for dir in d_temp:
-            print(dir)
-            for file in d_temp.get(dir):
-                print("\t", file)
+    l_temp = get_templates(temp_name)
+    l_dir = c_d(l_temp)
+    c_f(l_dir)
 
 
+crt_temp("app")
 # crt_files2(
 #     "py", "pope:pop2<py1:py:py3<asd:dwd<qwew:ewe>>>:asd1<asd2:asdf:asd3<q:2>:asd4>:asd5:asd52")
 
