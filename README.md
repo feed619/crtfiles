27e768ddf6176143f7ff358ea5b8666455ebf83e
feed228 <feed228new@gmail.com>
2024-07-03 11:46:54 +0300

    update setup.py

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 "# crtfiles"
 
-Описание библиотеки, как установить и использовать её.
+|pyversion|
+
+:Version: 1.0
+:Web: https://docs.celeryq.dev/en/stable/index.html
+:Download: https://pypi.org/project/celery/
+:Source: https://github.com/celery/celery/
+:Keywords: task, queue, job, async, rabbitmq, amqp, redis,
+python, distributed, actors
--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 # setup.py
 from setuptools import setup, find_packages
-NAME = "crt"
+NAME = "crtfiles"
 setup(
     name=NAME,
-    version='0.1',
+    version='1.0',
+    author="feed619",
+    author_email="azimovpro@gmail.com",
+    description="create files quickly and conveniently",
+    url="https://github.com/feed619/crtfiles",
     packages=find_packages(),
     install_requires=[
         'click',
@@ -17,4 +21,10 @@ setup(
     package_data={
         'crt': ['data/templates.json'],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
